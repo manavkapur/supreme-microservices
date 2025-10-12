e42f9b5e5693baf7d51564de4389da7d599594ec
Manav Kapur <62932155+manavkapur@users.noreply.github.com>
2025-10-12 23:34:57 +0530

    User-service

---
+++ user-service/src/main/java/com/supremesolutions/userservice/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.supremesolutions.userservice.repository;
+
+import com.supremesolutions.userservice.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+}
