fab56a155dc5fdd16f81d3a94d072d5cb1905f3e
Manav Kapur <62932155+manavkapur@users.noreply.github.com>
2025-10-13 00:17:05 +0530

    actuator setup

--- user-service/src/main/java/com/supremesolutions/userservice/config/SecurityConfig.java
+++ user-service/src/main/java/com/supremesolutions/userservice/config/SecurityConfig.java
@@ -25,7 +25,7 @@ public class SecurityConfig {
         http
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/users/register", "/api/users/login").permitAll()
+                        .requestMatchers("/api/users/register", "/api/users/login", "/actuator/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
