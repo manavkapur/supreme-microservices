f0195ca6120936d42f8c15d2b775657db9e46784
Manav Kapur <62932155+manavkapur@users.noreply.github.com>
2025-10-13 10:47:40 +0530

    user specific subscription

--- channel-server/src/main/java/com/supremesolutions/channel_server/dto/ChannelMessage.java
+++ channel-server/src/main/java/com/supremesolutions/channel_server/dto/ChannelMessage.java
@@ -1,3 +1,9 @@
 package com.supremesolutions.channel_server.dto;
 
-public record ChannelMessage(String from, String channelId, String content, long timestamp) {}
+public record ChannelMessage(
+        String from,
+        String channelId,
+        String content,
+        long timestamp,
+        String targetUser // 👈 optional — can be null for broadcast
+) {}
